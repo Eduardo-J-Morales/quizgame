079d88ec1a5bb0daf58d7aaa15e266315d7daccd
eduardojosegonzalezmorales41 <eduardojosegonzalezmorales41@gmail.com>
2024-11-15 22:44:15 +0000

    Already setting up the server for the json file before even making the actual json file

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
-
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "hola"
+def get_quiz_data():
+  with open('quiz_data.json', 'r') as f:
+    
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
